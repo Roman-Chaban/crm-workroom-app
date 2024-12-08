d3d3f04e48baea148bcec24daa78c4caa4c74e00
Roman Chaban <roman.chaban1001@gmail.com>
2024-12-08 19:04:22 +0200

    Updated DashboardPanel

--- src/components/home/DashboardPanel/DashboardPanel.tsx
+++ src/components/home/DashboardPanel/DashboardPanel.tsx
@@ -1,16 +1,34 @@
 'use client';
 
-import React, { type FC } from 'react';
+import React, { useMemo, type FC } from 'react';
 
 import { useParsedUserName } from '@/hooks/useParsedUserName';
 
 import { Container, Div, Heading } from '@/index/index';
 
 import styles from './DashboardPanel.module.scss';
+import Image from 'next/image';
 
 export const DashboardPanel: FC = () => {
   const sortedData = localStorage.getItem('registration');
   const userName = useParsedUserName(sortedData);
+
+  const formatDateRange = useMemo(() => {
+    const today = new Date();
+    const start = new Date(today.getFullYear(), today.getMonth(), 1);
+    const end = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+    const dateOptions: Intl.DateTimeFormatOptions = {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    };
+
+    const formattedStart = start.toLocaleDateString('en-US', dateOptions);
+    const formattedEnd = end.toLocaleDateString('en-US', dateOptions);
+
+    return `${formattedStart} - ${formattedEnd}`;
+  }, []);
 
   return (
     <Div className={styles['dashboard']}>
@@ -27,6 +45,20 @@ export const DashboardPanel: FC = () => {
           Dashboard
         </Heading>
       </Container>
+      <Div className={styles['dashboardDate']}>
+        <Image
+          src={'/icons/outlined-icons/calendar.svg'}
+          alt="Calendar Icon"
+          width={24}
+          height={24}
+        />
+        <Heading
+          tag="h5"
+          className={styles['dashboardDateTitle']}
+        >
+          {formatDateRange}
+        </Heading>
+      </Div>
     </Div>
   );
 };
